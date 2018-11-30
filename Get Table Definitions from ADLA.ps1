4027b30d7d4446d8568ad87f51e98b7f4b48d869
Sandeep Segu <segu.sandeep@gmail.com>
2018-11-30 13:39:55 -0500

    Add files via upload

---
+++ Get Table Definitions from ADLA.ps1
@@ -0,0 +1,17 @@
+﻿Login-AzureRmAccount
+
+$account = ""   #Enter ADLA account name
+$database = ""  #Enter ADLA database name
+$localpath = "C:\Temp\DevSPs"  #Choose your local path to download ADLA table definitions
+
+
+$ChildItems = Get-AzureRmDataLakeAnalyticsCatalogItem -Account $account -ItemType Table -Path "$database.dbo"
+
+    foreach ($ChildItem in $ChildItems) {
+    #$ChildItem
+        $script =  $childitem.ColumnList;
+        $file  = $ChildItem.Name;
+        $filename = "$file.txt";
+        $script | Out-File $localpath\$filename;
+        }
+    
